08ce93e7f3b634bd7ae0ac4fda3ac6889372ce89
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-09-01 21:42:28 +0530

    fixed cors error from frontend

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
-from routes.user import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(user, prefix="/user", tags=["User"])
+# Allow frontend origin(s)
+origins = [
+    "http://localhost:3000",  # React frontend
+    "http://127.0.0.1:3000",  # sometimes needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],          # allow all HTTP methods
+    allow_headers=["*"],          # allow all headers
+)
+
+# include your routers
+from routes.user import user
+app.include_router(user, prefix="/user", tags=["Users"])
